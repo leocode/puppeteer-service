194a6cede9d7fe2cae9b8bafb74b6651c486079e
JonaszJestem <jonaszwiacek@gmail.com>
2021-08-06 13:15:26 +0200

    fix: Switch comparison library

--- src/puppeteer.test.ts
+++ src/puppeteer.test.ts
@@ -1,63 +1,44 @@
 import { htmlToPng, urlToPng } from './puppeteer';
-import pixelmatch from 'pixelmatch';
-import fs from 'fs';
+import Jimp from 'jimp';
 import path from 'path';
 
 process.env.CHROME_BINARY_PATH = require('chromium-binary').path;
 
 const TEST_HTML = '<html><div>Test</div></html>';
 const TEST_HTML_URL = 'test/resources/test.html';
+const EXPECTED_PNG_FROM_TEST_HTML = 'test/resources/expected-html.png';
 
 const COMPARISON_THRESHOLD = 0.1;
 
 describe('Puppeteer Service', () => {
   it('should convert html to png', async () => {
-    const result = (await htmlToPng(TEST_HTML, {
+    const convertedPng = (await htmlToPng(TEST_HTML, {
       height: 200,
       width: 200,
     })) as Buffer;
 
-    const differentPixels = pixelmatch(
-      await getExpectedImage(),
-      result,
-      null,
-      getBufferDimensions(result.length),
-      getBufferDimensions(result.length),
-      {
-        threshold: COMPARISON_THRESHOLD,
-      },
+    const expectedImage = await Jimp.read(EXPECTED_PNG_FROM_TEST_HTML);
+    const actualImage = await Jimp.read(convertedPng);
+    expect(Jimp.distance(expectedImage, actualImage)).toBeLessThan(
+      COMPARISON_THRESHOLD,
     );
-
-    expect(differentPixels).toEqual(0);
   });
 
   it('should convert url to png', async () => {
     const url = `file://${path.resolve(TEST_HTML_URL)}`;
-    const result = (await urlToPng(url, {
+
+    const convertedPng = (await urlToPng(url, {
       height: 200,
       width: 200,
     })) as Buffer;
 
-    const differentPixels = pixelmatch(
-      await getExpectedImage(),
-      result,
-      null,
-      getBufferDimensions(result.length),
-      getBufferDimensions(result.length),
-      {
-        threshold: COMPARISON_THRESHOLD,
-      },
+    const expectedImage = await Jimp.read(EXPECTED_PNG_FROM_TEST_HTML);
+    const actualImage = await Jimp.read(convertedPng);
+    expect(Jimp.distance(expectedImage, actualImage)).toBeLessThan(
+      COMPARISON_THRESHOLD,
     );
-
-    expect(differentPixels).toEqual(0);
   });
 });
 
 const getBufferDimensions = (bufferLength: number) =>
   Math.round(Math.sqrt(bufferLength / 4));
-
-async function getExpectedImage() {
-  const EXPECTED_PNG_FROM_TEST_HTML = 'test/resources/expected-html.png';
-
-  return await fs.promises.readFile(EXPECTED_PNG_FROM_TEST_HTML);
-}
