53ebdcebb4265140c6a6303323d15848180d3af6
JonaszJestem <jonaszwiacek@gmail.com>
2021-08-10 14:43:40 +0200

    feat: Add PDF comparison tests

--- src/puppeteer.test.ts
+++ src/puppeteer.test.ts
@@ -1,12 +1,15 @@
-import { htmlToPng, urlToPng } from './puppeteer';
+import { htmlToPdf, htmlToPng, urlToPdf, urlToPng } from './puppeteer';
 import Jimp from 'jimp';
 import path from 'path';
+import fs from 'fs';
+import pdf from 'pdf-parse';
 
 process.env.CHROME_BINARY_PATH = require('chromium-binary').path;
 
 const TEST_HTML = '<html><div>Test</div></html>';
 const TEST_HTML_URL = 'test/resources/test.html';
 const EXPECTED_PNG_FROM_TEST_HTML = 'test/resources/expected-html.png';
+const EXPECTED_PDF_FROM_TEST_HTML = 'test/resources/expected.pdf';
 
 const COMPARISON_THRESHOLD = 0.1;
 
@@ -38,7 +41,29 @@ describe('Puppeteer Service', () => {
       COMPARISON_THRESHOLD,
     );
   });
-});
 
-const getBufferDimensions = (bufferLength: number) =>
-  Math.round(Math.sqrt(bufferLength / 4));
+  it('should convert html to pdf', async () => {
+    const expectedPdfFile = await fs.promises.readFile(
+      EXPECTED_PDF_FROM_TEST_HTML,
+    );
+    const expectedPdfContent = await pdf(expectedPdfFile);
+
+    const result = await htmlToPdf(TEST_HTML);
+
+    const actualPdfContent = await pdf(result);
+    expect(actualPdfContent.text).toEqual(expectedPdfContent.text);
+  });
+
+  it('should convert url to pdf', async () => {
+    const url = `file://${path.resolve(TEST_HTML_URL)}`;
+    const expectedPdfFile = await fs.promises.readFile(
+      EXPECTED_PDF_FROM_TEST_HTML,
+    );
+    const expectedPdfContent = await pdf(expectedPdfFile);
+
+    const result = await urlToPdf(url);
+
+    const actualPdfContent = await pdf(result);
+    expect(actualPdfContent.text).toEqual(expectedPdfContent.text);
+  });
+});
