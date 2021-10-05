159cfdb00f34dd05b98582f439f8c903c0d10236
Maciej Jaworski <mrjavorro@gmail.com>
2021-10-05 16:23:28 +0200

    refactor: remove mystery-var

--- src/url.utils.ts
+++ src/url.utils.ts
@@ -1,8 +1,7 @@
 import { config } from './config';
 
 export const validateUrl = (url: string) => {
-  const x = config();
-  if (!x.rejectFileProtocolUrls) {
+  if (!config().rejectFileProtocolUrls) {
     return;
   }
 
